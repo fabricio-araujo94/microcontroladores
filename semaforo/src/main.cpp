242573e0f24570c5712756a2c89706e050427f62
Fabricio <fabricio.araujo61@aluno.ifce.edu.br>
2024-10-30 14:42:25 -0300

    new project: semaphore + define pins

---
+++ semaforo/src/main.cpp
@@ -0,0 +1,20 @@
+#include <Arduino.h>
+#define CAR_LED_RED 21
+#define CAR_LED_GREEN 22
+#define CAR_LED_BLUE 23
+#define PEDESTRIAN_LED_RED 33
+#define PEDESTRIAN_LED_GREEN 32
+#define PEDESTRIAN_LED_BLUE 26
+
+// put function declarations here:
+
+void setup() {
+  // put your setup code here, to run once:
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
+
+// put function definitions here:
