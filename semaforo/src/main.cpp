c098e294ab3f5ff4cc789400abb88250269439c7
Fabricio <fabricio.araujo61@aluno.ifce.edu.br>
2024-10-30 14:47:42 -0300

    semaphores: carRun function

--- semaforo/src/main.cpp
+++ semaforo/src/main.cpp
@@ -7,6 +7,7 @@
 #define PEDESTRIAN_LED_BLUE 26
 
 // put function declarations here:
+void carRun();
 
 void setup() {
   // put your setup code here, to run once:
@@ -20,6 +21,15 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+  carRun();
 }
 
 // put function definitions here:
+void carRun() {
+  digitalWrite(CAR_LED_RED, LOW);
+  digitalWrite(CAR_LED_GREEN, HIGH);
+  digitalWrite(CAR_LED_BLUE, LOW);
+  digitalWrite(PEDESTRIAN_LED_RED, HIGH);
+  digitalWrite(PEDESTRIAN_LED_GREEN, LOW);
+  digitalWrite(PEDESTRIAN_LED_BLUE, LOW);
+}
