1c94bce524533928c7cf81de298e18c53adf9a10
Fabricio <fabricio.araujo61@aluno.ifce.edu.br>
2024-11-15 15:06:36 -0300

    new project: little fs

---
+++ little_fs/src/main.cpp
@@ -0,0 +1,27 @@
+#include <Arduino.h>
+#include <FS.h>
+#include <LittleFS.h>
+
+#define RED 21
+#define GREEN 19
+#define BLUE 18
+
+#define SSID ""
+#define PASSWORD ""
+
+// put function declarations here:
+
+void setup() {
+  // put your setup code here, to run once:
+
+  pinMode(RED, OUTPUT);
+  pinMode(GREEN, OUTPUT);
+  pinMode(BLUE, OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
+
+// put function definitions here:
