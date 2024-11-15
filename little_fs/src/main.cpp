9066e0e0c78089beed182370a7905a994102d12a
Fabricio <fabricio.araujo61@aluno.ifce.edu.br>
2024-11-15 19:23:23 -0300

    littlefs: listDir function

--- little_fs/src/main.cpp
+++ little_fs/src/main.cpp
@@ -11,6 +11,8 @@
 
 // put function declarations here:
 bool createDir(fs::FS &, const char *);
+void listDir(fs::FS &);
+void listDirAux(fs::FS &, const char *, uint8_t);
 
 void setup()
 {
@@ -40,4 +42,39 @@ bool createDir(fs::FS &fs, const char *path)
     Serial.println("Failed!");
     return false;
   }
+}
+
+void listDir(fs::FS &fs){
+  listDirAux(fs, "/", 1);
+}
+
+void listDirAux(fs::FS &fs, const char *dirname, uint8_t levels){
+    Serial.printf("Directory: %s\r\n", dirname);
+
+    File root = fs.open(dirname);
+    if(!root){
+        Serial.println("Failed!");
+        return;
+    }
+    if(!root.isDirectory()){
+        Serial.println("Not a directory.");
+        return;
+    }
+
+    File file = root.openNextFile();
+    while(file){
+        if(file.isDirectory()){
+            Serial.print("  DIR : ");
+            Serial.println(file.name());
+            if(levels){
+                listDirAux(fs, file.path(), levels -1);
+            }
+        } else {
+            Serial.print("  FILE: ");
+            Serial.print(file.name());
+            Serial.print("\tSIZE: ");
+            Serial.println(file.size());
+        }
+        file = root.openNextFile();
+    }
 }
