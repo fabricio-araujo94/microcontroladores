954e4d125555078edb7f843281c6a88045f9396c
Fabricio <fabricio.araujo61@aluno.ifce.edu.br>
2024-12-02 20:17:21 -0300

    esp_now_: the whole code at once again

--- esp_now_/src/main.cpp
+++ esp_now_/src/main.cpp
@@ -1,18 +1,137 @@
 #include <Arduino.h>
+#include <esp_now.h>
+#include <WiFi.h>
+#include <esp_wifi.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// LED Pin
+#define RED 18
+#define GREEN 19
+#define BLUE 21
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+// MY MAC: 84:cc:a8:5e:72:38
+uint8_t broadcastAddress[] = {0x84, 0xCC, 0xA8, 0x5E, 0x77, 0x44};
+
+typedef struct struct_message
+{
+  char a[32];
+  int b;
+  float c;
+  bool d;
+} struct_message;
+
+struct_message myData;
+esp_now_peer_info_t peerInfo;
+
+// void readMacAddress();
+void OnDataSent(const uint8_t *, esp_now_send_status_t);
+void OnDataSent(const uint8_t *, esp_now_send_status_t);
+
+void setup()
+{
+  Serial.begin(9600);
+
+  pinMode(RED, OUTPUT);
+  pinMode(GREEN, OUTPUT);
+  pinMode(BLUE, OUTPUT);
+
+  WiFi.mode(WIFI_STA);
+
+  if (esp_now_init() != ESP_OK)
+  {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+
+  esp_now_register_send_cb(esp_now_send_cb_t(OnDataSent));
+
+  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+  peerInfo.channel = 0;
+  peerInfo.encrypt = false;
+
+  if (esp_now_add_peer(&peerInfo) != ESP_OK)
+  {
+    Serial.println("Failed to add peer");
+    return;
+  }
+
+  esp_now_register_recv_cb(esp_now_recv_cb_t(OnDataRecv));
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  strcpy(myData.a, "blue");
+  myData.b = 3;
+  myData.c = 1.5;
+  myData.d = false;
+
+  // Send message via ESP-NOW
+  esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *)&myData, sizeof(myData));
+
+  if (result == ESP_OK)
+  {
+    Serial.println("Sent with success");
+  }
+  else
+  {
+    Serial.println("Error sending the data");
+  }
+  delay(2000);
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+/*
+void readMacAddress(){
+  uint8_t baseMac[6];
+  esp_err_t ret = esp_wifi_get_mac(WIFI_IF_STA, baseMac);
+  if (ret == ESP_OK) {
+    Serial.printf("%02x:%02x:%02x:%02x:%02x:%02x\n",
+                  baseMac[0], baseMac[1], baseMac[2],
+                  baseMac[3], baseMac[4], baseMac[5]);
+  } else {
+    Serial.println("Failed to read MAC address");
+  }
+}
+*/
+
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status)
+{
+  Serial.print("\r\nLast Packet Send Status:\t");
+  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+}
+
+void OnDataRecv(const uint8_t *mac, const uint8_t *incomingData, int len)
+{
+  memcpy(&myData, incomingData, sizeof(myData));
+
+  Serial.print("Bytes received: ");
+  Serial.println(len);
+  Serial.print("Char: ");
+  Serial.println(myData.a);
+  Serial.print("Int: ");
+  Serial.println(myData.b);
+  Serial.print("Float: ");
+  Serial.println(myData.c);
+  Serial.print("Bool: ");
+  Serial.println(myData.d);
+  Serial.println();
+
+  String color = String(myData.a);
+
+  if (color == "red")
+  {
+    digitalWrite(RED, HIGH);
+    digitalWrite(GREEN, LOW);
+    digitalWrite(BLUE, LOW);
+  }
+  else if (color == "green")
+  {
+    digitalWrite(RED, LOW);
+    digitalWrite(GREEN, HIGH);
+    digitalWrite(BLUE, LOW);
+  }
+  else if (color == "blue")
+  {
+    digitalWrite(RED, LOW);
+    digitalWrite(GREEN, LOW);
+    digitalWrite(BLUE, HIGH);
+  }
 }
