eb68c488f5a15796ac8afe8a71c6818efb589c27
Fabricio <fabricio.araujo61@aluno.ifce.edu.br>
2024-12-09 18:49:36 -0300

    pwm: rest of the code (it's quite simple)

--- pwm_/src/main.cpp
+++ pwm_/src/main.cpp
@@ -1,19 +1,40 @@
 #include <Arduino.h>
 
+#define RED 18
+#define GREEN 19
+#define BLUE 21
+
 // put function declarations here:
-int myFunction(int, int);
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  int result = myFunction(2, 3);
+  
+  pinMode(RED, OUTPUT);
+  pinMode(GREEN, OUTPUT);
+  pinMode(BLUE, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  
+  // yellow
+  analogWrite(RED, 255);
+  analogWrite(GREEN, 255);
+  analogWrite(BLUE, 0);
+  delay(3000);
+
+  // purple
+  analogWrite(RED, 158);
+  analogWrite(GREEN, 10);
+  analogWrite(BLUE, 149);
+  delay(3000);
+
+  // orange
+  analogWrite(RED, 251);
+  analogWrite(GREEN, 64);
+  analogWrite(BLUE, 3);
+  delay(3000);
 }
 
 // put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
