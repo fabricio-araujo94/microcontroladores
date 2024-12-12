54ba0ce2a517db5127938f94be5f6f68e1dc7ce5
Fabricio <fabricio.araujo61@aluno.ifce.edu.br>
2024-12-11 22:53:13 -0300

    ultrassom: the code itself

--- ultrassom/src/main.cpp
+++ ultrassom/src/main.cpp
@@ -1,14 +1,58 @@
 #include <Arduino.h>
 
+#define TRIG 5
+#define ECHO 18
+#define RED 19
+#define GREEN 21
+
+#define SOUND_SPEED 0.034
+#define CM_TO_INCH 0.393701
+
+long duration;
+float distanceCm;
+float distanceInch;
+
 // put function declarations here:
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+
+  pinMode(TRIG, OUTPUT);
+  pinMode(ECHO, OUTPUT);
+  pinMode(RED, OUTPUT);
+  pinMode(GREEN, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+  digitalWrite(TRIG, LOW);
+  delayMicroseconds(20);
+
+  digitalWrite(TRIG, HIGH);
+  delayMicroseconds(100);
+  digitalWrite(TRIG, LOW);
+
+  duration = pulseIn(ECHO, HIGH);
+
+  distanceCm = duration * SOUND_SPEED / 2;
+
+  Serial.print("Distance (cm): ");
+  Serial.println(distanceCm);
+
+  if(distanceCm <= 10) {
+    digitalWrite(RED, HIGH);
+    digitalWrite(GREEN, LOW);
+  } else if (distanceCm > 10 && distanceCm < 20) {
+    digitalWrite(RED, HIGH);
+    digitalWrite(GREEN, HIGH);
+  } else {
+    digitalWrite(RED, LOW);
+    digitalWrite(GREEN, HIGH);
+  }
+
+  delay(2000);
 }
 
 // put function definitions here:
